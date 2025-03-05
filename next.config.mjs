d376ad7ebaffacb54c87a6560af0a9d810adf39f
lucidframes <harikrishnanmr672@gmail.com>
2025-03-05 22:45:29 +0530

    Final Deployment Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Important for static export on Vercel
+    trailingSlash: true, // Optional, but helps avoid 404 issues
+  };
+  
+  export default nextConfig;
+  
